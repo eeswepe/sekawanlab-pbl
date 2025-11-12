166979951cdd69bfb57a14d2310270e63c5b0665
eeswepe <singgihwahyupermana@gmail.com>
2025-11-12 18:32:14 +0700

    fix: move login.php views

---
+++ src/Controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controller;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        $this->render("login");
+    }
+}
